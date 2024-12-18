19417c0befdf33f8e1d87d5e89f3b72b14a47121
Vasiliy Yakovlev <vasiliy.yakovlev@crystalrockstudios.com>
2024-12-18 20:16:01 +0300

    add coments

--- scripts/particleAnimation.js
+++ scripts/particleAnimation.js
@@ -1,47 +1,44 @@
 document.addEventListener("DOMContentLoaded", async () => {
     await window.tsParticles.load("tsparticles", {
       particles: {
-        number: {
+        number: { // количество частиц
           value: 44,
-          density: {
+          density: { // плотность частиц
             enable: true,
             value_area: 400
           }
         },
-        color: {
+        color: { // цвет частиц
           value: "#81b6ee"
         },
-        shape: {
+        shape: { // форма частиц
           type: "circle",
         },
-        opacity: {
-          value: 0.4,
-          random: true,
-          anim: {
-            enable: false,
-            speed: 1,
-            opacity_min: 0.1,
-            sync: false
+        opacity: { // прозрачность частиц
+          value: 0.6,
+          random: true, // рандомная прозрачность
+          anim: { // анимация плавного изменения прозрачности
+            enable: true,
+            speed: 0.1, // скорость анимации
+            opacity_min: 0.2, // минимальная прозрачность
           }
         },
-        size: {
-          value: 5,
-          random: true,
-          anim: {
-            enable: false,
-            speed: 40,
-            size_min: 0.1,
-            sync: false
+        size: { // размер частиц
+          value: 5, 
+          random: true, // рандомный размер
+          anim: { // анимация плавного изменения размера
+            enable: true,
+            speed: 0.1, // скорость анимации
+            size_min: 1, // минимальный размер
           }
         },
-        move: {
+        move: { // движение частиц
           enable: true,
-          speed: 1.5,
-          direction: "bottom",
-          random: false,
-          straight: false,
-          out_mode: "out",
-          bounce: false,
+          speed: 1.5, // скорость движения
+          direction: "bottom", // направление движения
+          random: false, // не случайное движение
+          straight: false, // не вертикально вниз движение
+          bounce: false, // не отскакивать от стенок
         }
       }
     });
